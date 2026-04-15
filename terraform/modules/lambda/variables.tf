873c0a58aea2cdf0cdc0fc49fc2020e24c8e9627
z31nnx <196381025+z31nnx@users.noreply.github.com>
2026-04-15 08:26:54 -1000

    docs: updated submodules

---
+++ terraform/modules/lambda/variables.tf
@@ -0,0 +1,31 @@
+variable "prefix" {
+  type = string
+}
+variable "function_name" {
+  type = string
+}
+variable "description" {
+  type = string
+  default = null
+}
+variable "role_arn" {
+  type = string
+}
+variable "runtime" {
+  type = string
+}
+variable "filename" {
+  type = string
+}
+variable "source_code_hash" {
+  type = string
+}
+variable "timeout" {
+  type = number
+}
+variable "memory_size" {
+  type = number
+}
+variable "publish" {
+  type = bool
+}
