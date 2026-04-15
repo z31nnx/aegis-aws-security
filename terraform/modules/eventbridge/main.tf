740411f5b97dff18d1e04705756cde5456ddfd20
z31nnx <196381025+z31nnx@users.noreply.github.com>
2026-04-15 10:08:45 -1000

    feart: eventbridge module

---
+++ terraform/modules/eventbridge/main.tf
@@ -0,0 +1,70 @@
+data "aws_iam_policy_document" "trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["scheduler.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = "${var.prefix}-${var.schedule_group_name}-schedule-role"
+  assume_role_policy = data.aws_iam_policy_document.trust.json
+
+  tags = {
+    Name = "${var.prefix}-${var.schedule_group_name}-eventbridge-role"
+  }
+}
+
+data "aws_iam_policy_document" "policy" {
+  dynamic "statement" {
+    for_each = var.role_policy
+    iterator = statement
+
+    content {
+      sid       = statement.value.sid
+      effect    = statement.value.effect
+      actions   = statement.value.actions
+      resources = statement.value.resources
+    }
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "${var.prefix}-${var.schedule_group_name}-scheduler-policy"
+  description = "${var.prefix}-${var.schedule_group_name}-scheduler-policy"
+  policy      = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_scheduler_schedule_group" "group" {
+  name = "${var.prefix}-${var.schedule_group_name}-group"
+
+  tags = {
+    Name = "${var.prefix}-${var.schedule_group_name}-group"
+  }
+}
+
+resource "aws_scheduler_schedule" "schedule" {
+  for_each = var.rules
+
+  name                = "${var.prefix}-${each.key}"
+  group_name          = aws_scheduler_schedule_group.group.name
+  state               = each.value.state
+  schedule_expression = each.value.schedule_expression
+
+  target {
+    arn      = each.value.target_arn
+    role_arn = aws_iam_role.role.arn
+  }
+
+  flexible_time_window {
+    mode = each.value.flexible_time_window
+  }
+}
