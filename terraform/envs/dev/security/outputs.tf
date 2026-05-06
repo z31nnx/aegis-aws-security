e95a62e4df4de84c1848243776f84fba6800e830
z31nnx <196381025+z31nnx@users.noreply.github.com>
2026-05-06 08:02:51 -1000

    feat: test instance module

--- terraform/envs/dev/security/outputs.tf
+++ terraform/envs/dev/security/outputs.tf
@@ -1,3 +1,9 @@
-output "iam_instance_profile" {
+output "ssm_instance_profile" {
   value = module.ssm_role.instance_profile_arn
+}
+
+output "sg_ids" {
+  value = {
+    ssm_sg = module.ssm_sg.sg_id
+  }
 }
--- terraform/envs/dev/testing/main.tf
+++ terraform/envs/dev/testing/main.tf
@@ -1,14 +1,24 @@
 data "terraform_remote_state" "security" {
   backend = "local"
   config = {
-    path = "../security/"
+    path = "../security/terraform.tfstate"
   }
+}
+
+module "" {
+  
 }
 
 module "test_instance" {
   source = "../../../modules/ec2"
   instance_name = "crypto-test"
+  instance_type = "t3.micro"
   force_destroy = false 
   associate_public_ip_address = true 
-  iam_instance_profile = 
+  key_name = null 
+  subnet_id = null
+  prefix = local.prefix
+  vpc_security_group_ids = [ data.terraform_remote_state.security.outputs.sg_ids.ssm_sg ]
+  iam_instance_profile = data.terraform_remote_state.security.outputs.ssm_instance_profile
+
 }
